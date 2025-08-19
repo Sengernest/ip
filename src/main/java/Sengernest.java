ceca9a58fa97aeb4db24502648af648926217ed6
Sengernest <sengernest69@gmail.com>
2025-08-19 23:44:17 +0800

    Level-1: Echo
    Improved so that it will echo any command entered by user and exits when
    user types "bye"

--- src/main/java/Sengernest.java
+++ src/main/java/Sengernest.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Sengernest {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Sengernest");
-        System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("What can I do for you?: ");
+
+        while(true) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println();
+                System.out.println("Goodbye, hope to see you again soon!");
+                break;
+            } else {
+                System.out.println();
+                System.out.println(command);
+                System.out.println();
+                System.out.print("Enter next command: ");
+            }
+        }
+
+        scanner.close();
     }
 }
