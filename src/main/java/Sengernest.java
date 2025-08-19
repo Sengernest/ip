5abd3038cfafa07ae2f5c325483e66fa9fbf214a
Sengernest <sengernest69@gmail.com>
2025-08-20 00:15:29 +0800

    Level 2: Add, List
    Stores users commands

--- src/main/java/Sengernest.java
+++ src/main/java/Sengernest.java
@@ -4,6 +4,8 @@ public class Sengernest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
         System.out.println("Hello! I'm Sengernest");
         System.out.print("What can I do for you?: ");
@@ -14,9 +16,22 @@ public class Sengernest {
                 System.out.println();
                 System.out.println("Goodbye, hope to see you again soon!");
                 break;
-            } else {
+            } else if (command.equals("list")) {
                 System.out.println();
-                System.out.println(command);
+                if (list[0] == null) {
+                    System.out.println("Nothing added yet!");
+                } else {
+                    for (int i = 0; i < count; i++) {
+                        System.out.println(i + 1 + ". "+ list[i]);
+                    }
+                }
+                System.out.println();
+                System.out.print("Enter next command: ");
+            } else {
+                list[count] = command;
+                count += 1;
+                System.out.println();
+                System.out.println("added to list: " + command);
                 System.out.println();
                 System.out.print("Enter next command: ");
             }
@@ -24,4 +39,5 @@ public class Sengernest {
 
         scanner.close();
     }
+
 }
